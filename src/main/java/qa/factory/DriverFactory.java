4c9d670c14c1456459fad434ea291a65045071a1
habdullahNelta <94700951+habdullahNelta@users.noreply.github.com>
2023-04-05 17:22:42 +0200

    Update DriverFactory.java

--- src/main/java/qa/factory/DriverFactory.java
+++ src/main/java/qa/factory/DriverFactory.java
@@ -32,7 +32,7 @@ public class DriverFactory {
 
         logger(Level.INFO,"browser value is: "+browser);
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
+     //   options.addArguments("--headless");
         options.addArguments("--remote-allow-origins=*");
         if (browser.equals("chrome")) {
             WebDriverManager.chromedriver().setup();
